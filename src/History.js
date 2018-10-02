2b95e747fe56c026c6dfa06434a9982df77ab786
julielm77 <julielyminh@gmail.com>
2018-10-02 10:26:28 +0200

    quete router

---
+++ src/History.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+
+
+const History = () => {
+    
+        return (
+            <div>
+               <p>History</p>
+            </div>
+        );
+    };
+
+
+export default History;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+
+const Home = () => {
+    
+        return (
+            <div>
+               <p><span class="bienvenue">Bienvenue</span> sur
+               l' <span class="accueil">accueil</span></p>
+            </div>
+        );
+    };
+
+
+export default Home;
